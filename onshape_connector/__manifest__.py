45aedb9fdeef91682bf07e34600b1276986c3db1
David Seitz <davidseitz8989@gmail.com>
2025-08-19 15:34:10 +0200

    Handle duplicate Onshape documents and log request errors

---
+++ onshape_connector/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Onshape Connector",
+    "version": "18.0.1.0.0",
+    "summary": "Sehr einfache Verbindung zu Onshape",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/onshape_document_views.xml"
+    ],
+    "license": "LGPL-3",
+    "application": False,
+}
