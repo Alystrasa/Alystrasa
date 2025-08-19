45eb049dbfc1219f91b33ecf5c7bc2c329f2426b
David Seitz <davidseitz8989@gmail.com>
2025-08-19 14:24:34 +0200

    Delete onshape_connector directory

--- onshape_connector/__manifest__.py
+++
@@ -1,7 +0,0 @@
-{
-    "name": "Onshape Connector",
-    "version": "1.0",
-    "depends": ["base"],
-    "data": ["data/ir_cron.xml"],
-    "installable": True,
-}
--- onshape_connector/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import onshape_document
